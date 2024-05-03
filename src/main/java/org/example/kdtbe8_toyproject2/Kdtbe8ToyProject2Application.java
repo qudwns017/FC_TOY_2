4724d94ed636ed1f8f427daff3d1be1862f46d3d
limeade23 <junhan.koo@limeade.me>
2024-05-03 20:54:39 +0900

    project setting

---
+++ src/main/java/org/example/kdtbe8_toyproject2/Kdtbe8ToyProject2Application.java
@@ -0,0 +1,13 @@
+package org.example.kdtbe8_toyproject2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Kdtbe8ToyProject2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Kdtbe8ToyProject2Application.class, args);
+    }
+
+}
