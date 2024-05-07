dc4c3a413e1e46147a692fe1d694c9919f33d8af
qudwns017 <qudwns017@naver.com>
2024-05-07 16:03:57 +0900

    Feat: getTripList(여행 리스트 조회) API 추가

--- src/main/java/org/example/kdtbe8_toyproject2/trip/controller/TripController.java
+++ src/main/java/org/example/kdtbe8_toyproject2/trip/controller/TripController.java
@@ -1,4 +1,33 @@
 package org.example.kdtbe8_toyproject2.trip.controller;
 
+import java.util.List;
+import org.example.kdtbe8_toyproject2.trip.db.TripMapper;
+import org.example.kdtbe8_toyproject2.trip.model.TripByIdDto;
+import org.example.kdtbe8_toyproject2.trip.model.TripListDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
 public class TripController {
+    @Autowired
+    private TripMapper mapper;
+
+    @GetMapping("/trips")
+    public ResponseEntity<?> getTripList() {
+        List<TripListDto> list = mapper.getTripList();
+        return new ResponseEntity<>(list, HttpStatus.OK);
+    }
+
+    @GetMapping("/trips/{id}")
+    public ResponseEntity<?> getTripById(@PathVariable int id) {
+        TripByIdDto trip = mapper.getTripById(id);
+        return new ResponseEntity<>(trip, HttpStatus.OK);
+    }
 }
--- src/main/java/org/example/kdtbe8_toyproject2/trip/db/TripMapper.java
+++ src/main/java/org/example/kdtbe8_toyproject2/trip/db/TripMapper.java
@@ -1,9 +1,12 @@
 package org.example.kdtbe8_toyproject2.trip.db;
 
-import java.awt.print.Book;
 import java.util.List;
+import org.apache.ibatis.annotations.Mapper;
+import org.example.kdtbe8_toyproject2.trip.model.TripByIdDto;
+import org.example.kdtbe8_toyproject2.trip.model.TripListDto;
 
+@Mapper
 public interface TripMapper {
-    List<TripEntity> getTripList();
-    void getTrip(); // 반환 자료형 수정 필요
+    List<TripListDto> getTripList();
+    TripByIdDto getTripById(int id); // 반환 자료형 수정 필요
 }
