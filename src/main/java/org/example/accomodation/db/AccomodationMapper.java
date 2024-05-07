e8856db3114bdd46c1895412957adc5a4922a211
fullm <matoma1_@naver.com>
2024-05-07 15:20:12 +0900

    feat/ interface AccomodationMapper

--- src/main/java/org/example/accomodation/db/AccomodationMapper.java
+++ src/main/java/org/example/accomodation/db/AccomodationMapper.java
@@ -5,5 +5,7 @@ import org.example.accomodation.model.AccomodationRequest;
 import java.util.List;
 
 public interface AccomodationMapper {
-
+    public void delete(Long tripId, Long accomodationId);
+    public List<AccomodationEntity> findByTripId(Long tripId, Long accomodationId);
+    public AccomodationEntity create(AccomodationRequest accomodationRequest);  //void
 }
