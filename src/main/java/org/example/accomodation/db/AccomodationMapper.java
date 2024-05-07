ef7d1bb01b79c662d1be6cd4d49d669f8970159a
fullm <matoma1_@naver.com>
2024-05-07 17:00:10 +0900

    fix/create 수정

--- src/main/java/org/example/accomodation/db/AccomodationMapper.java
+++ src/main/java/org/example/accomodation/db/AccomodationMapper.java
@@ -1,11 +1,13 @@
 package org.example.accomodation.db;
 
-import org.example.accomodation.model.AccomodationRequest;
+import org.example.accomodation.model.AccomodationDTO;
 
 import java.util.List;
 
 public interface AccomodationMapper {
     public void delete(Long tripId, Long accomodationId);
-    public List<AccomodationEntity> findByTripId(Long tripId, Long accomodationId);
-    public AccomodationEntity create(AccomodationRequest accomodationRequest);  //void
+   // public List<AccomodationEntity> findByTripId(Long tripId, Long accomodationId);
+    public int create(AccomodationEntity accomodationEntity);
+    public List<AccomodationEntity> findByTripId(Long tripId);
+    AccomodationDTO toAccomodationDto(List<AccomodationEntity> entity);
 }
--- src/main/java/org/example/accomodation/service/AccomodationService.java
+++ src/main/java/org/example/accomodation/service/AccomodationService.java
@@ -3,6 +3,7 @@ package org.example.accomodation.service;
 import lombok.RequiredArgsConstructor;
 import org.example.accomodation.db.AccomodationEntity;
 import org.example.accomodation.db.AccomodationMapper;
+import org.example.accomodation.model.AccomodationDTO;
 import org.example.accomodation.model.AccomodationRequest;
 import org.springframework.stereotype.Service;
 
@@ -12,22 +13,37 @@ import java.util.List;
 @RequiredArgsConstructor
 public class AccomodationService {
     private final AccomodationMapper accomodationMapper;
-    //private final TripMapper tripMapper;
+    private final AccomodationConverter accomodationConverter;
 
-    public AccomodationEntity create(AccomodationRequest accomodationRequest) {
-        return accomodationMapper.create(accomodationRequest);
+    public void create(Long trip_id,AccomodationRequest accomodationRequest) {
+        var entity = AccomodationEntity.builder()
+                .tripId(trip_id)
+                .accomodationId(accomodationRequest.getTripId())
+                .name(accomodationRequest.getName())
+                .checkInDatetime(accomodationRequest.getCheckInDatetime())
+                .checkOutDatetime(accomodationRequest.getCheckOutDatetime())
+                .build();
+        var saveEntity = accomodationMapper.create(entity);
+
     }
 
     public void delete(Long tripId, Long accomodationId) {
          accomodationMapper.delete(tripId,accomodationId);
     }
 
-    public List<AccomodationEntity> findByTripId(Long tripId, Long accomodationId){
-        return accomodationMapper.findByTripId(tripId, accomodationId);
+
+
+    public AccomodationDTO findByTripId(Long tripId){
+        var entity =  accomodationMapper.findByTripId(tripId);
+        return accomodationMapper.toAccomodationDto(entity);
     }
+
 }
 /*
 create()
 findByTripId()
 delete()
  */
+/*  public List<AccomodationEntity> findByTripId(Long tripId, Long accomodationId){
+        return accomodationMapper.findByTripId(tripId, accomodationId);
+    }*/
