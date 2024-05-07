b71bb7599574a3d4fbc5c25dedc6f33b76bef0eb
qudwns017 <qudwns017@naver.com>
2024-05-07 14:11:10 +0900

    Refactor: TripMapper Class -> Interface

--- src/main/java/org/example/kdtbe8_toyproject2/trip/db/TripMapper.java
+++ src/main/java/org/example/kdtbe8_toyproject2/trip/db/TripMapper.java
@@ -1,4 +1,9 @@
 package org.example.kdtbe8_toyproject2.trip.db;
 
-public class TripMapper {
+import java.awt.print.Book;
+import java.util.List;
+
+public interface TripMapper {
+    List<TripEntity> getTripList();
+    void getTrip(); // 반환 자료형 수정 필요
 }
