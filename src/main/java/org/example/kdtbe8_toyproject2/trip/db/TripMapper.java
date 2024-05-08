9ac8bc70765a089b83827d8cdb1ec98a902214c6
fullm <matoma1_@naver.com>
2024-05-08 17:46:45 +0900

    feat/ trip

---
+++ src/main/java/org/example/kdtbe8_toyproject2/trip/db/TripMapper.java
@@ -0,0 +1,4 @@
+package org.example.kdtbe8_toyproject2.trip.db;
+
+public class TripMapper {
+}
---
+++ src/main/java/org/example/kdtbe8_toyproject2/trip/model/TripDto.java
@@ -0,0 +1,13 @@
+package org.example.kdtbe8_toyproject2.trip.model;
+
+import java.time.LocalDateTime;
+
+public class TripDto {
+    private Long tripId;
+    private String tripName;
+    private LocalDateTime startDate;
+    private LocalDateTime endDate;
+    private Boolean isOversea;
+//    private List<AccomodationDto>;
+//    private List<ItineraryDto>;
+}
