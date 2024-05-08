7ddd3c3c271bc287950a28fbbe6ee1a4eff482db
cbjoon <qudwns017@naver.com>
2024-05-08 16:25:06 +0900

    Rename: TripAccommodationDto -> GetTripByIdDto 클래스명 변경

--- src/main/java/org/example/kdtbe8_toyproject2/trip/model/GetTripByIdDto.java
+++ src/main/java/org/example/kdtbe8_toyproject2/trip/model/GetTripByIdDto.java
@@ -9,7 +9,7 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class TripAccommodationDto {
+public class GetTripByIdDto {
     private Long id;
     private String name;
     private LocalDateTime startDate;
