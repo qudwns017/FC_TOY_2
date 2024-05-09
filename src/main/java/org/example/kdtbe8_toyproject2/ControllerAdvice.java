06cbd78319fdd65ccf1b3702882140956ad5df49
GOren98 <gangmin9812@gmail.com>
2024-05-09 15:10:56 +0900

    feat : exception 처리 기능 추가

--- src/main/java/org/example/kdtbe8_toyproject2/ControllerAdvice.java
+++ src/main/java/org/example/kdtbe8_toyproject2/ControllerAdvice.java
@@ -7,8 +7,11 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.sql.SQLIntegrityConstraintViolationException;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
+import java.util.Stack;
 
 @RestControllerAdvice
 @Slf4j
@@ -24,5 +27,13 @@ public class ControllerAdvice {
 
         log.error("failure check request validation, uri: {}, objectName: {}, {}", request.getRequestURI(), objectName, errors);
         return ResponseEntity.badRequest().body(errorDefaultMessages);
+    }
+
+    @ExceptionHandler(SQLIntegrityConstraintViolationException.class)
+    public ResponseEntity<ApiResponseError> handleValidationExceptions(SQLIntegrityConstraintViolationException e, HttpServletRequest request) {
+        RuntimeException error = new RuntimeException(e);
+
+        log.error("failure check request validation, uri: {}, objectName: {}, {}", request.getRequestURI());
+        return ResponseEntity.badRequest().body(ApiResponseError.of(error));
     }
 }
